b6da852b8585c581bcadb8bba534bc1b89b70c67
NoobMaster669 <NoobMaster669@protonmail.com>
2022-01-24 10:42:32 +0530

    POC

---
+++ POC/bittorrent_v2.py
@@ -0,0 +1,6 @@
+import os
+# import torrentfile
+
+print("test")
+# os.system(f"mktorrent -v -p -l 23 -c \"Torrent created by GG-Bot Upload Assistant\" -s '{source}' -a '{announce[0]}' -o \"{working_folder}/temp_upload/{tracker}-{torrent_info['torrent_title']}.torrent\" \"{media}\"")
+# torrentfile.create()
