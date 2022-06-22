d27c534a25040d4742f3b1afe68c6c7441c7c4e2
NoobMaster669 <NoobMaster669@protonmail.com>
2022-06-22 23:41:33 +0530

    adding dev scripts

---
+++ dev_scripts/latest_mediainfo/ubuntu_20.04.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+curl -O https://mediaarea.net/download/binary/libzen0/0.4.39/libzen0v5_0.4.39-1_amd64.xUbuntu_20.04.deb
+dpkg -i libzen0v5_0.4.39-1_amd64.xUbuntu_20.04.deb
+rm libzen0v5_0.4.39-1_amd64.xUbuntu_20.04.deb
+
+curl -O https://mediaarea.net/download/binary/libmediainfo0/22.03/libmediainfo0v5_22.03-1_amd64.xUbuntu_20.04.deb
+dpkg -i libmediainfo0v5_22.03-1_amd64.xUbuntu_20.04.deb
+rm libmediainfo0v5_22.03-1_amd64.xUbuntu_20.04.deb
+
+curl -O https://mediaarea.net/download/binary/mediainfo/22.03/mediainfo_22.03-1_amd64.xUbuntu_20.04.deb
+dpkg -i mediainfo_22.03-1_amd64.xUbuntu_20.04.deb
+rm mediainfo_22.03-1_amd64.xUbuntu_20.04.deb
